e134f9f5cc0179a5d68f9811df0ba42689fb51bf
Amanda Dieuaide <amanda.dieuaide@eleve.isep.fr>
2021-04-21 22:04:10 +0200

    Contact Form for the average user : - Added mailing (SMTP server)

--- Psy_count/contact.php
+++ Psy_count/contact.php
@@ -1,35 +1,32 @@
 <?php
-echo "<pre>";
-print_r($_POST);
-
 $msg_envoi = false;
 
-try{
-    if (isset($_POST["mail_Cct"]) && $_POST["mail_Cct"] != '') { //Check if we have an email
+try {
+    if (isset($_POST['submit']) && isset($_POST["mail_Cct"]) && $_POST["mail_Cct"] != '') { //Check if we have an email
         if (filter_var($_POST["mail_Cct"], FILTER_VALIDATE_EMAIL)) { //check si c'est une adresse email
             $visitorFirstName = $_POST["prenom_Cct"];
             $visitorLastName = $_POST["nom_Cct"];
             $visitorEmail = $_POST["mail_Cct"];
             $visitorMsgSubject = $_POST["msgSubject_Cct"];
             $visitorMsg = $_POST["msg_Cct"];
-    
-            $to = "amanda.dieuaide@isep.fr";
+
+            $to = "amanda.dieuaide@gmail.com"; //Get mail from Admin
             $body = "";
-    
+
             $body .= "De : " . $visitorFirstName . "\r\n";
             $body .= "Email : " . $visitorEmail . "\r\n";
             $body .= "Message : " . $visitorMsg . "\r\n";
-    
-            // mail($to, $visitorMsgSubject, $body);
+
+            ini_set('sendmail_from', $visitorEmail);
+            ini_set('display_errors', 1);
+            error_reporting(E_ALL);
+            //mail($to, $visitorMsgSubject, $body);
             $msg_envoi = true;
         }
     }
-} catch (Exception $e){
+} catch (Exception $e) {
     echo "Erreur :", $e->getMessage(), "\n";
 }
-
-
-echo "</pre>";
 ?>
 
 
@@ -52,95 +49,110 @@ echo "</pre>";
 </header>
 
 <body>
+    
+<!--Faire un truc plus clean avec du JS
+    <//?php
+        if (isset($_POST['submit']) && $msg_filled != true) { 
+        if (empty($visitorEmail) || empty($visitorFirstName) || empty($visitorLastName) || empty($visitorMsgSubject) || empty($visitorMsg)) {
+        ?>
+            <div id="form">
+                <h3 id="headerText"> Vous n'avez pas complété tous les champs ! </h3>
+            </div>
+    <//?php 
+    }
+    } 
 
+    ?>
+-->
     <?php
     if ($msg_envoi) :
     ?>
-        <h3 id="headerText"> Message bien reçu ! Nous vous recontacterons prochainement.</h2>
-
-        <?php
+        <div id="form">
+            <h3 id="headerText"> Message bien reçu ! Nous vous recontacterons prochainement.</h2>
+        </div>
+    <?php
     else :
-        ?>
+    ?>
 
 
-            <div id="form" class="form_content">
-                <form action="" method="POST">
-                    <div><label id="form" class="form_label" for="text"> Prénom </label>
-                        <input id="form" class="form_content" type="text" name="prenom_Cct" placeholder="ex : John"> </label>
-                    </div>
+        <div id="form" class="form_content">
+            <form action="contact.php" method="POST" autocomplete="off">
+                <div><label id="form" class="form_label" for="text"> Prénom </label>
+                    <input id="form" class="form_content" type="text" name="prenom_Cct" placeholder="ex : John"> </label>
+                </div>
 
-                    <div><label id="form" class="form_label" for="text"> Nom </label>
-                        <input id="form" class="form_content" type="text" name="nom_Cct" placeholder="ex : Doe"> </label>
-                    </div>
+                <div><label id="form" class="form_label" for="text"> Nom </label>
+                    <input id="form" class="form_content" type="text" name="nom_Cct" placeholder="ex : Doe"> </label>
+                </div>
 
-                    <div> <label id="form" class="form_label" for="text"> E-mail </label>
-                        <input id="form" class="form_content" type="text" name="mail_Cct" placeholder="ex : john.doe@gmail.com"> </label>
-                    </div>
+                <div> <label id="form" class="form_label" for="text"> E-mail </label>
+                    <input id="form" class="form_content" type="text" name="mail_Cct" placeholder="ex : john.doe@gmail.com"> </label>
+                </div>
 
-                    <div> <label id="form" class="form_label" for="text"> Sujet du message </label>
-                        <!--Faire un menu déroulant ?-->
-                        <input id="form" class="form_content" type="text" name="msgSubject_Cct" placeholder="ex : Contact avec l'administrateur PSY-fi..."> </label>
-                    </div>
+                <div> <label id="form" class="form_label" for="text"> Sujet du message </label>
+                    <!--Faire un menu déroulant ?-->
+                    <input id="form" class="form_content" type="text" name="msgSubject_Cct" placeholder="ex : Contact avec l'administrateur PSY-fi..."> </label>
+                </div>
 
-                    <div>
-                        <label id="form" class="form_label" for="text"> Message </label>
-                        <textarea id="form" class="form_content" name="msg_Cct" placeholder="Veuillez écrire votre message..."></textarea>
-                    </div>
+                <div>
+                    <label id="form" class="form_label" for="text"> Message </label>
+                    <textarea id="form" class="form_content" name="msg_Cct" placeholder="Veuillez écrire votre message..."></textarea>
+                </div>
 
-                    <div>
-                        <button id="form" type="submit"> Envoyer </button>
-                    </div>
-                </form>
-            </div>
+                <div>
+                    <button id="form" type="submit" name="submit"> Envoyer </button>
+                </div>
+            </form>
+        </div>
 
-        <?php
+    <?php
     endif;
-        ?>
+    ?>
 
-        <!-- v Should be in a footer :/ mais la mise en page marche pas?-->
-        <div id="container4">
+    <!-- v Should be in a footer :/ mais la mise en page marche pas?-->
+    <div id="container4">
 
-            <div id="groupInformation">
-                <span style="color:white">
-                    <li>
-                        <h2>PSY-FI</h2>
-                    </li>
-                    <li>28 Rue Notre Dame des Champs, 75006 Paris<br></li>
-                    <li>01 23 45 67 89</li>
+        <div id="groupInformation">
+            <span style="color:white">
+                <li>
+                    <h2>PSY-FI</h2>
+                </li>
+                <li>28 Rue Notre Dame des Champs, 75006 Paris<br></li>
+                <li>01 23 45 67 89</li>
 
-                </span>
-            </div>
+            </span>
+        </div>
 
 
-            <div id="groupeSettings">
-                <span style="color:white">
-                    <li>
-                        <h2> Accessibilité </h2>
-                    </li>
-                    <li> Thème</li>
-                    <li> Langue</li>
-                </span>
-
-            </div>
-
-            <div id="groupeRessources">
-                <span style="color:white">
-                    <li>
-                        <h2> Ressources </h2>
-                    </li>
-                    <li> Actualités </li>
-                    <li> FAQ </li>
-                    <li> Videos </li>
-                </span>
-
-            </div>
-
+        <div id="groupeSettings">
+            <span style="color:white">
+                <li>
+                    <h2> Accessibilité </h2>
+                </li>
+                <li> Thème</li>
+                <li> Langue</li>
             </span>
 
         </div>
 
+        <div id="groupeRessources">
+            <span style="color:white">
+                <li>
+                    <h2> Ressources </h2>
+                </li>
+                <li> Actualités </li>
+                <li> FAQ </li>
+                <li> Videos </li>
+            </span>
 
-        <?php include("footer.php") ?>
+        </div>
+
+        </span>
+
+    </div>
+
+
+    <?php include("footer.php") ?>
 </body>
 
 <footer>
