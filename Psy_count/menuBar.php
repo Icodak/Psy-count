e6846d06f9401833b082dfad95e22ffbb5526db1
Eclypso2 <73545745+Eclypso2@users.noreply.github.com>
2021-04-29 15:08:51 +0200

    add messagerie to menu

--- Psy_count/menuBar.php
+++ Psy_count/menuBar.php
@@ -47,6 +47,7 @@ session_start();
 
     <a href="faq.php" class="menuText"> FAQ </a>
     <a href="accueil.php#menuBarAnchor" class="menuText"> A propos </a>
+    <a href="msg_interne.php" class="menuText"> Messagerie (en test) </a>
     <?php
     if (!isset($_SESSION['connexion']) || $_SESSION['connexion'] != 1) {
       //Le formulaire de Contact normal est utilisable par les visiteurs
