29568cfe3164aefc183539e077ef13cf95171cf3
Icodak <24356594+Icodak@users.noreply.github.com>
2021-05-03 16:48:31 +0200

    correct SEO homepage

--- Psy_count/accueil.php
+++ Psy_count/accueil.php
@@ -6,23 +6,26 @@
 	<title>Accueil</title>
 	<link rel="icon" type="image/png" href="images/psy-fi.png" />
 	<link rel="stylesheet" href="css/styleAccueil.css">
-	<link rel="stylesheet" href="css/styles_fonts.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 
 <body>
-<div class="menuBar1">
-	<div>
+
+	<div class="menu-bar">
+		<div>
 			<?php include("menuBar.php") ?>
-			</div>
-</div>
+		</div>
+	</div>
+
+
 	<section class="banner">
 
 		<div>
-			<h1 class="headerText"> PSY-Count</h1>
+			<h1 class="header-text">PSY-Count</h1>
 		</div>
-		<div class="headerDescription">
-			<p class="headerPresentation"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed<br>
+		<div class="header-description">
+			<p class="header-presentation"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed<br>
 				do eiusmod tempor incididunt ut labore et dolore magna<br>
 				aliqua.
 			</p>
@@ -30,61 +33,55 @@
 		<div>
 			<button class="button">Découvrir</button>
 		</div>
+		
 	</section>
 
-	<section class="container1">
+
+	<section class="section-1">
+
 		<h2>Nos objectifs</h2>
-		<hr size=3 class="separator">
-		<div class="block1">
-			<div class="column1">
-				<div><img src="images/Placement_Area_ASSETsmallSIZED.png" alt="" /></div>
-				<div class="abstractText">
-					<h2>Bilan de santé </h2>
-				</div>
-				<div>
-					<p> Lorem ipsum dolor sit amet, consectetur<br>
+		<hr>
+		<div class="section-1-main">
+			<div class="section-1-column">
+				<img src="images/Placement_Area_ASSETsmallSIZED.png" alt="Photographie santé" />
+				<h2>Bilan de santé </h2>
+				<p> Lorem ipsum dolor sit amet, consectetur<br>
 					adipiscing elit, sed do eiusmod tempor incididunt <br>
 					ut labore et dolore magna aliqua.
-					</p>
-				</div >
-				<div  class="En-savoir-plus"><button class="button">En savoir plus</button></div>
+				</p>
+				<div><button class="button">En savoir plus</button></div>
 			</div>
-			<div class="column1">
-				<div><img src="images/Placement_Area_ASSETsmallSIZED_di.png" alt="" /></div>
-				<div class="abstractText">
-					<h2>Soutien des hopitaux</h2>
-				</div>
-				<div>
-					<p>  Lorem ipsum dolor sit amet, consectetur<br>
+			<div class="section-1-column">
+				<img src="images/Placement_Area_ASSETsmallSIZED_di.png" alt="Photographie hôpital" />
+				<h2>Soutien des hopitaux</h2>
+				<p> Lorem ipsum dolor sit amet, consectetur<br>
 					adipiscing elit, sed do eiusmod tempor incididunt <br>
 					ut labore et dolore magna aliqua.
-					</p>
-				</div >
-				<div class="En-savoir-plus"><button class="button">En savoir plus</button></div>
+				</p>
+				<div><button class="button">En savoir plus</button></div>
 			</div>
-			<div class="column1">
-				<div><img src="images/Placement_Area_ASSETsmallSIZED_dy.png" alt="" /></div>
-				<div class="abstractText">
-					<h2>Suivi des seniors</h2>
-				</div>
-				<div>
-					<p>  Lorem ipsum dolor sit amet, consectetur<br>
+			<div class="section-1-column">
+				<img src="images/Placement_Area_ASSETsmallSIZED_dy.png" alt="Photographie seniors heureux" />
+				<h2>Suivi des seniors</h2>
+				<p> Lorem ipsum dolor sit amet, consectetur<br>
 					adipiscing elit, sed do eiusmod tempor incididunt <br>
 					ut labore et dolore magna aliqua.
-					</p>
-				</div>
-				<div  class="En-savoir-plus"><button class="button">En savoir plus</button></div>
+				</p>
+				<div><button class="button">En savoir plus</button></div>
 			</div>
-		</div>
 	</section>
 
 
-	<section class="container2">
-		<video class="video1" controls poster="images/Placement_Area_ASSETpanelSTATE@2x.png" src="video/SEP_V0_P1.mp4" type="video/mp4"></video>
+	<section class="section-2">
+
+		<video class="section-2-video" controls poster="images/Placement_Area_ASSETpanelSTATE@2x.png" src="video/SEP_V0_P1.mp4" type="video/mp4"></video>
+
 	</section>
 
-	<section class="container3">
-		<div class="descriptifText white-text">
+
+	<section class="section-3">
+
+		<div class="section-3-text white-text">
 			<h2>Notre histoire</h2>
 			<p>Natus error sit voluptatem accusantium<br>
 				doloremque laudantium, totam rem.</p>
@@ -98,31 +95,54 @@
 				labore et dolore magnam aliquam quaerat voluptatem.
 			</p>
 		</div>
-		<div class="presentation1">
-			<div class="who">
-				<h2>Qui sommes nous ?</h2>
-			</div>
-			<hr size=3 class="separator">
-			<div class="discordImages">
-				<div class="discordGroup">
-					<div class="discord-description"><img src="images/nicolas.jpg"><p>Nicolas</p></div>
-					<div class="discord-description"><img src="images/victoria.jpg"><p>Victoria</p></div>
-					<div class="discord-description"><img src="images/justin.jpg"><p>Justin</p></div>
+
+		<div class="section-3-people">
+
+			<h2>Qui sommes nous ?</h2>
+			<hr>
+			<div class="psy-fi-creator-section">
+				<div class="psy-fi-creator-group">
+					<div class="psy-fi-creator-description">
+						<img class="psy-fi-creator-picture" src="images/nicolas.jpg" alt="Nicolas profil">
+						<p>Nicolas</p>
+					</div>
+					<div class="psy-fi-creator-description">
+						<img class="psy-fi-creator-picture" src="images/victoria.jpg" alt="Victoria profil">
+						<p>Victoria</p>
+					</div>
+					<div class="psy-fi-creator-description">
+						<img class="psy-fi-creator-picture" src="images/justin.jpg" alt="Justin profil">
+						<p>Justin</p>
+					</div>
 				</div>
-				<div class="discordGroup">
-					<div class="discord-description"><img src="images/amanda.jpg"><p>Amanda</p></div>
-					<div class="discord-description"><img src="images/arsene.jpg"><p>Arsène</p></div>
-					<div class="discord-description"><img src="images/thibault.jpg"><p>Thibault</p></div>
+				<div class="psy-fi-creator-group">
+					<div class="psy-fi-creator-description">
+						<img class="psy-fi-creator-picture" src="images/amanda.jpg" alt="Amanda profil">
+						<p>Amanda</p>
+					</div>
+					<div class="psy-fi-creator-description">
+						<img class="psy-fi-creator-picture" src="images/arsene.jpg" alt="Arsène profil">
+						<p>Arsène</p>
+					</div>
+					<div class="psy-fi-creator-description">
+						<img class="psy-fi-creator-picture" src="images/thibault.jpg" alt="Thibault profil">
+						<p>Thibault</p>
+					</div>
 				</div>
 			</div>
 		</div>
+
 		<div class="psy-icon">
-			<img class="imagePsyFi" src="images/psy-fi.png" />
+			<img class="image-psy-fi" src="images/psy-fi.png" />
 		</div>
+
 	</section>
 
-	<section class="container4 white-text" id="menuBarAnchor">
+
+	<section class="section-4 white-text">
+
 		<?php include("footer.php") ?>
+
 	</section>
 </body>
 
