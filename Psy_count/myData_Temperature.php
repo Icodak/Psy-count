4cadd7fbab3e2677c542ddee9ad90c6edac9c59f
Amanda Dieuaide <amanda.dieuaide@eleve.isep.fr>
2021-05-04 20:03:21 +0200

    Added piechart and linechart (JS toggle to check)

--- Psy_count/myData_Temperature.php
+++ Psy_count/myData_Temperature.php
@@ -1,10 +1,4 @@
 <?php
-$x = 800;
-$y = 500;
-$marge = 50;
-$intX = ($x - (2 * $marge)) / 12;
-$intY = ($y - (2 * $marge)) / 10;
-
 $dbData = new PDO("mysql:host=localhost;dbname=serveur_psy_fi", 'root', '');
 $dbData->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -28,7 +22,8 @@ for ($i = 0; $i <= count($values) - 1; $i++) {
     print_r($array_data_value);
 }
 
-$testGraph = fopen('temp_test.json', 'w');
+//Test :  envoi des données sur un fichier json
+/*$testGraph = fopen('temp_test.json', 'w');
 fwrite($testGraph, json_encode($values));
 fclose($testGraph);
 
@@ -39,39 +34,7 @@ fclose($fp);
 $fp2 = fopen('temp_value.json', 'w');
 fwrite($fp2, json_encode($array_data_value));
 fclose($fp2);
-
-
-//Librairy needed
-/*$img = imagecreatetruecolor($x, $y);
-$noir = imagecolorallocate($img, 0, 0, 0);
-$blanc = imagecolorallocate($img, 255, 255, 255);
-$orange = imagecolorallocate($img, 220, 100, 0);
-$gris = imagecolorallocate($img, 220, 220, 220);
-
-imagefill($img, 0, 0, $blanc);
-imageline($img, $marge, $y - $marge, $x - $marge, $y - $marge, $noir);
-imageline($img, $marge, $y - $marge, $marge, $marge, $noir);
-imagettftext($img, 20, 0, $x - $marge - 10, $y - $marge + 30, $noir, "css/fonts/POPPINS-BLACK.ttf", "Date");
-imagettftext($img, 20, 0, $marge - 45, $marge - 25, $noir, "css/fonts/POPPINS-BLACK.ttf", "Températures");
-for ($i = 0; $i <= 10; $i++) {
-    imageline($img, $marge - 2, $y - $marge - ($i * $intY), $marge + 2, $y - $marge - ($i * $intY), $noir);
-    imagettftext($img, 10, 0, $marge - 45, $y - $marge - ($i * $intY), $noir, "css/fonts/POPPINS-BLACK.ttf", $i * 100);
-    if ($i > 0)
-        imageline($img, $marge + 2, $y - $marge - ($i * $intY), $x - $marge, $y - $marge - ($i * $intY), $gris);
-}
-for ($i = 0; $i < 12; $i++) {
-    imageline($img, $marge + $i * $intX, $y - $marge - 2, $marge + $i * $intX, $y - $marge + 2, $noir);
-    imagettftext($img, 10, -45, $marge + $i * $intX, $y - $marge + 20, $noir, "css/fonts/POPPINS-BLACK.ttf", $array_data_date[$i]);
-
-    imagesetthickness($img, 1);
-    if ($i < 11) {
-        imageline($img, $marge + $i * $intX + 1, $y - $marge - ($array_data_value[$i] * ($y - 2 * $marge) / 1000), $marge + ($i + 1) * $intX + 1, $y - $marge - ($array_data_value[$i + 1] * ($y - 2 * $marge) / 1000), $orange);
-    }
-    imagefilledellipse($img, $marge + $i * $intX + 1, $y - $marge - ($array_data_value[$i] * ($y - 2 * $marge) / 1000), 10, 10, $noir);
-}
-imagettftext($img, 12, 0, $marge + $i * $intX + 8, $y - $marge - ($array_data_value[$i] * ($y - 2 * $marge) / 1000) - 10, $orange, "css/fonts/POPPINS-BLACK.ttf", $array_data_value[$i]);
 */
-//$resultats=array(); //GET VALUE FROM BDD
 ?>
 
 <!doctype html>
@@ -85,6 +48,61 @@ imagettftext($img, 12, 0, $marge + $i * $intX + 8, $y - $marge - ($array_data_va
     <link rel="stylesheet" href="css/style_myData_Data.css">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
     <script type="text/javascript" src="javascript//javaScriptCode.js"></script>
+
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        window.addEventListener('resize', reportWindowSize);
+
+        function reportWindowSize() {
+            //alert("Window size has changed!");
+            //document.getElementById('linechart').reload();
+        }
+    </script>
+
+    <script type="text/javascript">
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+
+        function chart(chartType) {
+
+            var data = google.visualization.arrayToDataTable([
+                ['data_date', 'valeurs'],
+                <?php
+                for ($j = 0; $j <= count($array_data_date) - 1; $j++) {
+                    echo "['" . $array_data_date[$j] . "'," . $array_data_value[$j] . "],";
+                }
+                ?>
+            ]);
+
+            if (chartType === 'pieChart') { //inverser data pour piechart
+                var options = {
+                    title: "Graphe de l'évolution de la température superficielle",
+                };
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+            } else if (chartType === 'lineChart') {
+                var options = {
+                    title: "Courbe de l'évolution de la température superficielle",
+                    curveType: 'function',
+                    legend: {
+                        position: 'bottom'
+                    }
+                };
+                var chart = new google.visualization.LineChart(document.getElementById('linechart'));
+            }
+
+            chart.draw(data, options);
+
+            //Afficher/Désafficher
+            if (chart.display === "none") {
+                document.querySelector("#piechart").hidden = true;
+
+            } else if (chart.display === "block") {
+                document.querySelector("#piechart").hidden = false;
+            }
+        }
+    </script>
 </head>
 
 <body>
@@ -93,13 +111,15 @@ imagettftext($img, 12, 0, $marge + $i * $intX + 8, $y - $marge - ($array_data_va
         <?php include("menuBar.php") ?>
     </header>
 
-    <div class="light-background">
-        <div class="form">
-            <h1>Test</h1>
+    <div class="form">
+        <h1>Test</h1>
+        <p>Veuillez cliquer sur le choix d'affichage qui vous convient le mieux :</p>
+        <input type="button" onclick="chart('pieChart')" value="Voir mes données sous forme de camembert">
+        <div id="piechart"></div>
 
-        </div>
+        <input type="button" onclick="chart('lineChart')" value="Voir mes données sous forme de courbe d'évolution">
+        <div id="linechart"></div>
     </div>
-
 
     <?php include("footer.php") ?>
 
