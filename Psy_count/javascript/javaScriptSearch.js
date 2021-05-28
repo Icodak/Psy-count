55268306e12c5bb6281db5f1bec093924e639996
Icodak <24356594+Icodak@users.noreply.github.com>
2021-05-28 18:35:33 +0200

    update style for search bar

--- Psy_count/javascript/javaScriptSearch.js
+++ Psy_count/javascript/javaScriptSearch.js
@@ -10,8 +10,14 @@ function buildSearchBar(div) {
     prenom_div.appendChild(prenom_list);
     prenom_div.classList = "search-div";
     prenom_list.classList = "search-list";
-    
-    prenom_input.oninput = function() {
+
+    function cleanList() {
+        while (prenom_list.firstChild) {
+            prenom_list.firstChild.remove()
+        }
+    }
+
+    prenom_input.oninput = function () {
         $.ajax({
             url: "searchQuery.php",
             type: "GET",
@@ -23,19 +29,20 @@ function buildSearchBar(div) {
         })
             .done(function (result) {
                 //clean list
-                while (prenom_list.firstChild) {
-                    prenom_list.firstChild.remove()
-                }
+                cleanList();
                 //populate list
                 for (res of result) {
-                    (function() {
-                    var prenom_li_txt = document.createTextNode(res[0]);
-                    var prenom_li = document.createElement("li");
-                    prenom_li.onclick = function() {prenom_input.value = prenom_li.textContent;};
-                    console.log(prenom_li);
-                    prenom_li.appendChild(prenom_li_txt);
-                    prenom_list.appendChild(prenom_li);
-                })()
+                    (function () {
+                        var prenom_li_txt = document.createTextNode(res[0]);
+                        var prenom_li = document.createElement("li");
+                        prenom_li.onclick = function () {
+                            prenom_input.value = prenom_li.textContent;
+                            cleanList();
+                        };
+                        console.log(prenom_li);
+                        prenom_li.appendChild(prenom_li_txt);
+                        prenom_list.appendChild(prenom_li);
+                    })()
                 }
                 console.log(result);
             })
@@ -46,6 +53,10 @@ function buildSearchBar(div) {
 
 
     };
+
+    prenom_list.onblur = function () {
+        cleanList();
+    }
 
     div.appendChild(prenom_p);
     div.appendChild(prenom_div);
