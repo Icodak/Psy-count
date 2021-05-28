7ead9b0a4370cb61a99d362294491931f34e354f
Icodak <24356594+Icodak@users.noreply.github.com>
2021-05-28 17:28:13 +0200

    fonction build search bar

---
+++ Psy_count/javascript/javaScriptSearch.js
@@ -0,0 +1,53 @@
+function buildSearchBar(div) {
+
+    var prenom_txt = document.createTextNode("Prénom");
+    var prenom_p = document.createElement("p");
+    prenom_p.appendChild(prenom_txt);
+    var prenom_div = document.createElement("div");
+    var prenom_input = document.createElement("input");
+    var prenom_list = document.createElement("ul");
+    prenom_div.appendChild(prenom_input);
+    prenom_div.appendChild(prenom_list);
+    prenom_div.classList = "search-div";
+    prenom_list.classList = "search-list";
+    
+    prenom_input.oninput = function() {
+        $.ajax({
+            url: "searchQuery.php",
+            type: "GET",
+            dataType: 'JSON',
+            data: {
+                search_value: prenom_input.value + "%",
+                search_type: `prenom`
+            }
+        })
+            .done(function (result) {
+                //clean list
+                while (prenom_list.firstChild) {
+                    prenom_list.firstChild.remove()
+                }
+                //populate list
+                for (res of result) {
+                    (function() {
+                    var prenom_li_txt = document.createTextNode(res[0]);
+                    var prenom_li = document.createElement("li");
+                    prenom_li.onclick = function() {prenom_input.value = prenom_li.textContent;};
+                    console.log(prenom_li);
+                    prenom_li.appendChild(prenom_li_txt);
+                    prenom_list.appendChild(prenom_li);
+                })()
+                }
+                console.log(result);
+            })
+            .fail(function (xhr, thrownError) {
+                console.log(xhr);
+                console.log(thrownError);
+            });
+
+
+    };
+
+    div.appendChild(prenom_p);
+    div.appendChild(prenom_div);
+
+}
