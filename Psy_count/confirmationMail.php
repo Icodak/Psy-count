0bf6cb4c2c9786ab10d174646c2515b585a31b38
Eclypso2 <73545745+Eclypso2@users.noreply.github.com>
2021-05-29 22:56:55 +0200

    p

--- Psy_count/confirmationMail.php
+++ Psy_count/confirmationMail.php
@@ -5,16 +5,15 @@ if(isset($_POST['code']))
 
 
 try{
+  $from = "tullinnicolas@gmail.com";
+  $to = "tullinnicolas@gmail.com";
+  $subject = "Essai de PHP Mail ";
+  $message = "Votre Code de verification est ";
+  $headers = "De :" . $from;
 	ini_set('sendmail_from', 'tullinnicolas@gmail.com');
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "tullinnicolas@gmail.com";
-    $to = "tullinnicolas@gmail.com";
-    $subject = "Essai de PHP Mail ";
-    $message = "Votre Code de verification est ";
-    $headers = "De :" . $from;
-    mail($to,$subject,$message, $headers);
-    echo 'message envoyé';
+  ini_set( 'display_errors', 1 );
+  error_reporting( E_ALL );
+  mail($to,$subject,$message, $headers);
 }
 catch(PDOException $e){
         echo "Erreur : " . $e->getMessage();
